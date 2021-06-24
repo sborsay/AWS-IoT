11d3d9513d31c1c2888706040e9c96114df544f3
Stephen Borsay <borsay@gmail.com>
2021-06-23 20:24:12 -0700

    Update WioLTE-to-Soracom-DHT11.ino

--- SoracomWioLTE/WioLTE-to-Soracom-DHT11.ino
+++ SoracomWioLTE/WioLTE-to-Soracom-DHT11.ino
@@ -1,1 +1,203 @@
-hh
+#include <WioLTEforArduino.h>
+#include <WioLTEClient.h>
+#include <PubSubClient.h>    // https://github.com/SeeedJP/pubsubclient
+#include <stdio.h>
+
+#define SENSOR_PIN    (WIOLTE_D38)
+
+#define APN               "soracom.io"
+#define USERNAME          "sora"
+#define PASSWORD          "sora"
+
+#define MQTT_SERVER_HOST  "beam.soracom.io"
+#define MQTT_SERVER_PORT  (1883)
+
+#define ID                "WioLTE"
+#define OUT_TOPIC         "outTopic"
+#define IN_TOPIC          "inTopic"
+
+#define INTERVAL          (60000)
+
+WioLTE Wio;
+WioLTEClient WioClient(&Wio);
+PubSubClient MqttClient;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  SerialUSB.print("Subscribe:");
+  for (int i = 0; i < length; i++) SerialUSB.print((char)payload[i]);
+  SerialUSB.println("");
+}
+
+void setup()
+{
+  TemperatureAndHumidityBegin(SENSOR_PIN);//only thing added in setup
+  
+  delay(200);
+
+  SerialUSB.println("");
+  SerialUSB.println("--- START ---------------------------------------------------");
+  
+  SerialUSB.println("### I/O Initialize.");
+  Wio.Init();
+  
+  SerialUSB.println("### Power supply ON.");
+  Wio.PowerSupplyLTE(true);
+  delay(500);
+
+  SerialUSB.println("### Turn on or reset.");
+  if (!Wio.TurnOnOrReset()) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+
+  SerialUSB.println("### Connecting to \""APN"\".");
+  if (!Wio.Activate(APN, USERNAME, PASSWORD)) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+
+  SerialUSB.println("### Connecting to MQTT server \""MQTT_SERVER_HOST"\"");
+  MqttClient.setServer(MQTT_SERVER_HOST, MQTT_SERVER_PORT);
+  MqttClient.setCallback(callback);
+  MqttClient.setClient(WioClient);
+  if (!MqttClient.connect(ID)) {
+    SerialUSB.println("### ERROR! ###");
+    return;
+  }
+  MqttClient.subscribe(IN_TOPIC);
+
+  SerialUSB.println("### Setup completed.");
+}
+
+void loop()
+{
+  delay(200);
+  float temp;
+  float humi;
+  
+  //If you need to increase buffer size, you need to change MQTT_MAX_PACKET_SIZE in PubSubClient.h
+  char envDataBuf[128]; //local data buffer
+
+  if (!TemperatureAndHumidityRead(&temp, &humi)) {
+    SerialUSB.println("ERROR!");
+    goto err;
+  }
+
+
+  SerialUSB.print("Current humidity = ");
+  SerialUSB.print(humi);
+  SerialUSB.print("%  ");
+  SerialUSB.print("temperature = ");
+  SerialUSB.print(temp);
+  SerialUSB.println("C");
+
+  sprintf(envDataBuf, "{\"uptime\":%lu,\"temperature\":%f,\"humidity\":%f}", millis() / 1000, temp, humi);
+
+  SerialUSB.print("Publish:");
+  SerialUSB.print(envDataBuf);
+  SerialUSB.println("");
+  MqttClient.publish(OUT_TOPIC, envDataBuf); //send data to beam
+
+
+
+err:
+  delay(2000);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+
+int TemperatureAndHumidityPin;
+
+void TemperatureAndHumidityBegin(int pin)
+{
+  TemperatureAndHumidityPin = pin;
+  DHT11Init(TemperatureAndHumidityPin);
+}
+
+bool TemperatureAndHumidityRead(float* temperature, float* humidity)
+{
+  byte data[5];
+  
+  DHT11Start(TemperatureAndHumidityPin);
+  for (int i = 0; i < 5; i++) data[i] = DHT11ReadByte(TemperatureAndHumidityPin);
+  DHT11Finish(TemperatureAndHumidityPin);
+
+
+  if(!DHT11Check(data, sizeof (data))) return false;
+  if (data[1] >= 10) return false;
+  if (data[3] >= 10) return false;
+
+  *humidity = (float)data[0] + (float)data[1] / 10.0f;
+  *temperature = (float)data[2] + (float)data[3] / 10.0f;
+
+  
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+
+void DHT11Init(int pin)
+{
+  digitalWrite(pin, HIGH);
+  pinMode(pin, OUTPUT);
+}
+
+void DHT11Start(int pin)
+{
+  // Host the start of signal
+  digitalWrite(pin, LOW);
+  delay(18);
+  
+  // Pulled up to wait for
+  pinMode(pin, INPUT);
+  while (!digitalRead(pin)) ;
+  
+  // Response signal
+  while (digitalRead(pin)) ;
+  
+  // Pulled ready to output
+  while (!digitalRead(pin)) ;
+}
+
+byte DHT11ReadByte(int pin)
+{
+  byte data = 0;
+  
+  for (int i = 0; i < 8; i++) {
+    while (digitalRead(pin)) ;
+
+    while (!digitalRead(pin)) ;
+    unsigned long start = micros();
+
+    while (digitalRead(pin)) ;
+    unsigned long finish = micros();
+
+    if ((unsigned long)(finish - start) > 50) data |= 1 << (7 - i);
+  }
+  
+  return data;
+}
+
+void DHT11Finish(int pin)
+{
+  // Releases the bus
+  while (!digitalRead(pin)) ;
+  digitalWrite(pin, HIGH);
+  pinMode(pin, OUTPUT);
+}
+
+bool DHT11Check(const byte* data, int dataSize)
+{
+  if (dataSize != 5) return false;
+
+  byte sum = 0;
+  for (int i = 0; i < dataSize - 1; i++) {
+    sum += data[i];
+  }
+
+  return data[dataSize - 1] == sum;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
