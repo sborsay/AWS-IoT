28e7556658f391f00150cfaaf93b9eb1feb0aac8
Stephen Borsay <borsay@gmail.com>
2022-01-23 11:26:07 -0800

    Update ESP8266_AWS_PubSub.py

--- MicroPython/ESP8266_AWS_PubSub.py
+++ MicroPython/ESP8266_AWS_PubSub.py
@@ -10,29 +10,28 @@ import machine
 pin = machine.Pin(2)
 
 #This works for either ESP8266 ESP32 if you rename certs before moving into /flash 
-CERT_FILE = "/myCert.cert.der"
-KEY_FILE = "/myKey.key.der"
+CERT_FILE = "/<Your-DER-Converted-Client-Cert>"
+KEY_FILE = "/<Your-DER-Converted-Private-Key>"
 
 #if you change the ClientId make sure update AWS policy
-MQTT_CLIENT_ID = "bas638"
-MQTT_PORT = 8883
+MQTT_CLIENT_ID = "badfhs638"
+MQTT_PORT = 8883 #MQTT Secured
 
 #if you change the topic make sure update AWS policy
 PUB_TOPIC = "outTopic" #coming out of device
 SUB_TOPIC = "inTopic"  #coming into device
 
 #Change the following three settings to match your environment
-MQTT_HOST = "IKnowKungFuees-ats.iot.us-west-2.amazonaws.com"
-WIFI_SSID = "iPhone"
-WIFI_PW = "burger888"
+MQTT_HOST = "IKnowKungFuees-ats.iot.us-west-2.amazonaws.com"  #Your AWS IoT Endpoint IoT Core-->Settings
+WIFI_SSID = "<Your-WiFi-Network>"
+WIFI_PW = "<Your-WiFi-Password"
 
-MQTT_CLIENT = None
+MQTT_CLIENT = None  #empty obecjt
 
-print("h3fhf")
-#do_connect()
+print("Starting program...")
 
 def do_connect():
-    print("h3fhf")
+    print("In connect method")
     import network
     print("hfhf")
     sta_if = network.WLAN(network.STA_IF)
@@ -58,22 +57,6 @@ def sub_cb(topic, msg):
     print('Device received a Message: ')
     print((topic, msg))  #print incoing message asychronously
     pin.value(0) #blink if incoming message by toggle off
-     #if topic == "inTopic" and msg == b'received':
-        #print('ESP received hello message')
-    
-
-#def sub_msg():    #Subscribe incoming message is asynchronously delivered 
- #   global MQTT_CLIENT
- #   try:
- #   MQTT_CLIENT.set_callback(sub_cb)
- #       MQTT_CLIENT.subscribe(SUB_TOPIC)
- #       print('Subscribed to %s topic' % (SUB_TOPIC))
-        #print("Received: " + msg)
- #   except Exception as e:
- #       print("Exception subscribe: " + str(e))
- #       raise
-
-    
 
 def connect_mqtt():    
     global MQTT_CLIENT
@@ -92,7 +75,6 @@ def connect_mqtt():
         MQTT_CLIENT = MQTTClient(client_id=MQTT_CLIENT_ID, server=MQTT_HOST, port=MQTT_PORT, keepalive=5000, ssl=True, ssl_params={"cert":cert, "key":key, "server_side":False})
         MQTT_CLIENT.connect()
         print('MQTT Connected')
-        #sub_msg() #set up subscription and wait for incoming messages, subsciption is set up below
         MQTT_CLIENT.set_callback(sub_cb)
         MQTT_CLIENT.subscribe(SUB_TOPIC)
         print('Subscribed to %s as the incoming topic' % (SUB_TOPIC))
@@ -100,7 +82,6 @@ def connect_mqtt():
     except Exception as e:
         print('Cannot connect MQTT: ' + str(e))
         raise
-
 
 #start execution
 try:
@@ -112,21 +93,13 @@ try:
     print("cc")
     while True: #loop forever
             pin.value(1)
-            #new_message =  MQTT_CLIENT.check_msg()
             new_message = MQTT_CLIENT.check_msg()  # check for new subsciption payload incoming
             if new_message != 'None':  #check if we have a message and continue to publish, if so then get the message
-        #if new_message != 'None':     #without using this check we can easily miss incoming messages
-                temp = (time.time())%99#random.randint(0, 100)
-                humid = (time.time())%98#random.randint(0, 100)
+                temp = (time.time())%99   #random.randint(0, 100)  because randomint doesnt work
+                humid = (time.time())%98  #random.randint(0, 100)
                 deviceTime = time.time()
-                #temp= random.seed(int(time.time()))
-                #temp = int( time.time() * 1000.0 )
-                #x= int(random.seed(10))
-                #y=urandom.seed(22)
-                #temp = int(random.getrandbits(x))
-                #{\n  \"temperature\": 55,\n  \"humidity\": 66,\n  \"timestamps\": 12345\n}
                 pub_msg("{\n  \"temperature\": %d,\n  \"humidity\": %d,\n  \"timestamps\": %d\n}"%(temp,humid,deviceTime))
-                print("OK")
+                print("OK Published payload")
                 time.sleep(5)  # 5 second delay between publishing
             
 except Exception as e:
