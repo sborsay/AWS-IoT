090ad620da6cf2cb8ba538551e3823bacbf02dc8
Stephen Borsay <borsay@gmail.com>
2022-02-12 11:46:43 -0800

    Update MQTTfx_Script.js

--- MQTTfx_Script.js
+++ MQTTfx_Script.js
@@ -1,14 +1,17 @@
+//MQTT.fx script fountain, modified by Stephen Borsay
+
 var Thread = Java.type("java.lang.Thread");
 
-var topic = "hhh"
+var topic = "outTopic";
 var waitTime = 2000;
 var iterations = 10;
 
+
 function execute(action) {
     out("Test Script: " + action.getName());
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < iterations; i++) {
         sendPayload();
-        Thread.sleep(1000);
+        Thread.sleep(waitTime);
     }
     action.setExitCode(0);
     action.setResultText("done.");
@@ -17,23 +20,22 @@ function execute(action) {
 }
 
 function sendPayload() {
-		
-	var temp = Math.round(Math.random()*130);
-	var humid = Math.round(Math.random()*100);
-	var ts = Date.now();
 	
-	var IoT_Payload = { 
-  "temperature" :  temp, 
-  "humidity"    :  humid, 
-  "timestamps"  :  ts
-}
+    var temp = Math.round(Math.random()*130);
+    var humid = Math.round(Math.random()*100);
+    var ts = Date.now();
 
-var payload = JSON.stringify(IoT_Payload)
+    var IoT_Payload = { 
+	  "temperature" :  temp, 
+	  "humidity"    :  humid, 
+	  "timestamps"  :  ts
+	}
+
+    var payload = JSON.stringify(IoT_Payload)
 
     out("payload sent");
-    mqttManager.publish("home/garden/fountain", payload);
+    mqttManager.publish(topic, payload);
 }
-
 
 function out(message){
      output.print(message);
