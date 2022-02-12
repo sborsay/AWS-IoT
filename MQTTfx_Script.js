477e96d5084fd1ae27e039414cb933d1df536fb9
Stephen Borsay <borsay@gmail.com>
2022-02-12 11:36:17 -0800

    Update MQTTfx_Script.js

--- MQTTfx_Script.js
+++ MQTTfx_Script.js
@@ -1,34 +1,14 @@
-
 var Thread = Java.type("java.lang.Thread");
 
 var topic = "hhh"
 var waitTime = 2000;
 var iterations = 10;
 
-
-var temp= Math.random()*130;
-var humid = Math.random()*100;
-var ts = Date.now();
-
-var IoT_Payload = { 
-  "temperature" :  temp, 
-  "humidity"    :  humid, 
-  "timestamps"  :  ts
-}
-
-var payload = JSON.stringify(IoT_Payload)
-out(payload)
-//out(sammy)
-//var IoT_Payload = JSON.parse(sammy.data);
-//out(IoT_Payload)
-
 function execute(action) {
     out("Test Script: " + action.getName());
     for (var i = 0; i < 10; i++) {
-        switchON(payload);
-        Thread.sleep(500);
-        switchOFF();
-        Thread.sleep(500);
+        sendPayload();
+        Thread.sleep(1000);
     }
     action.setExitCode(0);
     action.setResultText("done.");
@@ -36,17 +16,24 @@ function execute(action) {
     return action;
 }
 
-function switchON(myPayload) {
-    out("fountain ON");
-    mqttManager.publish("home/garden/fountain", "ON");
-	//var IoT_Payload = JSON.parse(sammy);
-	mqttManager.publish("home/garden/fountain", myPayload);
+function sendPayload() {
+		
+	var temp = Math.round(Math.random()*130);
+	var humid = Math.round(Math.random()*100);
+	var ts = Date.now();
+	
+	var IoT_Payload = { 
+  "temperature" :  temp, 
+  "humidity"    :  humid, 
+  "timestamps"  :  ts
 }
 
-function switchOFF() {
-    out("fountain OFF");
-    mqttManager.publish("home/garden/fountain", "OFF");
+var payload = JSON.stringify(IoT_Payload)
+
+    out("payload sent");
+    mqttManager.publish("home/garden/fountain", payload);
 }
+
 
 function out(message){
      output.print(message);
